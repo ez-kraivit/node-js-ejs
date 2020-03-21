cb1f4a36e6ae73f57e7c62d69a408c63a65fc6fd
Mr.doocode <58212260+ez-kraivit@users.noreply.github.com>
2020-03-21 16:22:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ var https = require('https');
 var fs = require('fs');
 var express = require('./config/express');
 var app = express();
-
+const PORT = process.env.PORT || 8100
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+//   }, app)
+//   .listen(6020, function () {
+//     console.log('Example app listening on port 3000! Go to https://localhost:6020/')
+//   })
 https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
-  }, app)
-  .listen(6020, function () {
-    console.log('Example app listening on port 3000! Go to https://localhost:6020/')
-  })
-
+  }, app).listen(process.env.PORT,function(){
+    console.log('Start Server At port ${PORT}')
+})
 // app.listen(6020);
 module.exports = app;
 
